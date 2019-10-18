7fcd6d585f3435cf9d616e1f47afc7b5b7c58501
Captain Merica!! <stone0352@yahoo.com>
2019-10-17 22:15:49 -0500

    Update DiffieHellman.js

--- Code/DiffieHellman.js
+++ Code/DiffieHellman.js
@@ -1,16 +1,3 @@
-/*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Ctrl+R),
- * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
- * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
- */
-//---------------------------------  https://en.wikipedia.org/wiki/Diffie%E2%80%93Hellman_key_exchange
-
-
-
-
 // Note: This still needs logic to confirm that both Secret keys are equivilant for Bob and Alice   if(S_Bob == S_Alice){/*open secure connection*/};
 class Bob {
   constructor(bob, public_prime_base, public_prime_modulus, bob_priv_key, alice_pub_key, bob_pub_key, s_key_Alice, s_key_Bob){
