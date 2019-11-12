bd9923ca78a58ea61222dbe8a2dca8ba9388ba46
zhusain <motorz@gmail.com>
2019-11-12 11:32:13 -0600

    Hashing function called simpleHash.

--- Code/Backend/hashing.js
+++ Code/Backend/hashing.js
@@ -1,47 +1,29 @@
-//TUTORIAL on javascript classes:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
 
 class Hashing {
-    constructor(plaintext) {
-      this.plaintext = plaintext;
-      //this.hash = hash;
-    }
-  
-    // Getter
-    get hash() {
-      return this.hashFunc();
-    }
     // 
-    hashFunc() {
-
-        const crypto = require('crypto');
-        const hash = crypto.createHash('sha256');
-        
-        hash.on('readable', () => {
-            // Only one element is going to be produced by the
-            // hash stream.
-            const data = hash.read();
-            if (data) {
-              console.log(data.toString('hex'));
-              // Prints:
-              //   6a2da20943931e9834fc12cfe5bb47bbd9ae43489a30726962b576f4e3993e50
-            }
-          });
-
-        hash.write('some data to hash');
-        hash.end();
-
-        return this.hash;
-
-    }
+    simpleHash(msg) {
+      var hashNum = 7;
+  
+      for (var i = 0; i < msg.length; i++) {
+          hashNum = (hashNum * 31) + msg.charCodeAt(i);
+      }
+      var hashString = hashNum.toString();
+  
+      while ((hashString.length % 16) != 0) {
+          hashString = hashString + '0';
+      }
+      return hashString;
+  }
     
   }
   
   export default class Hashing {}
   
   /* USAGE in another file (like main.js)
-  const Symmetric = require("./symmetric.js");    //includes the class in other file
-  const symEncryp = new Symmetric(ADWADADAWDWA, 10);  //make a new class variable
+  const Hashing = require("./hashing.js");    //includes the class in other file
+  const hash = new Hashing;  // make a new class variable
+  var hashedMessage = hash.simpleHash("message");
   
   console.log(symEncryp.ciphertext); // does the thing
   */
