ca08add54ec2344929ae8ce0b028f350fd258d3f
Robbie Castillo <jaygrazer@gmail.com>
2019-11-15 22:24:28 -0600

    Added readme

--- Code/Backend/hashing.js
+++ Code/Backend/hashing.js
@@ -1,37 +1,34 @@
-
-
 class Hashing {
-    // 
-    simpleHash(msg) {
-      var hashNum = 0;
-      var prime = 7;
-      
-      for (var i = 0; i < msg.length; i++) {
-        hashNum = hashNum + (prime * msg.charCodeAt(i));
-        //console.log(hashNum);
-      }
+  //
+  simpleHash(msg) {
+    var hashNum = 0;
+    var prime = 7;
 
-      hashNum = hashNum % 5953474341373129;
+    for (var i = 0; i < msg.length; i++) {
+      hashNum = hashNum + prime * msg.charCodeAt(i);
+      //console.log(hashNum);
+    }
 
-      var hashString = hashNum.toString();
+    hashNum = hashNum % 5953474341373129;
 
-      while(hashString.length < 16){
-        hashNum = hashNum * 2;
-        hashString = hashNum.toString();
-      }
+    var hashString = hashNum.toString();
 
-      if(hashString.length > 16){
-        hashString = hashString.substring(0,16);
-      }
+    while (hashString.length < 16) {
+      hashNum = hashNum * 2;
+      hashString = hashNum.toString();
+    }
 
-      return hashString;
-    } 
-    
+    if (hashString.length > 16) {
+      hashString = hashString.substring(0, 16);
+    }
+
+    return hashString;
   }
-  
-  module.exports = Hashing
-  
-  /* USAGE in another file (like main.js)
+}
+
+module.exports = Hashing;
+
+/* USAGE in another file (like main.js)
   const Hashing = require("./hashing.js");    //includes the class in other file
   const hash = new Hashing;  // make a new class variable
   var hashedMessage = hash.simpleHash("message");
